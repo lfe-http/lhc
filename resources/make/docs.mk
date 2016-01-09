53f2f1d9e24421ec4209d71224b0783ed16e75ee
Duncan McGreggor <oubiwann@gmail.com>
2016-01-09 14:24:23 -0600

    Generated content.

--- resources/make/docs.mk
+++ resources/make/docs.mk
@@ -1,34 +1,33 @@
 .PHONY: docs
 REPO = $(shell git config --get remote.origin.url)
-DOCS_DIR = docs
+DOCS_DIR = $(shell pwd)/docs
 DOCS_BUILD_DIR = $(DOCS_DIR)/build
 DOCS_PROD_DIR = $(DOCS_DIR)/master
 SLATE_GIT_HACK = $(DOCS_DIR)/.git
 
 $(SLATE_GIT_HACK):
-	cd $(DOCS_DIR) && ln -s ../.git .
+		cd $(DOCS_DIR) && ln -s ../.git .
 
 docs-setup:
-	cd docs && bundle install
+		cd docs && bundle install
 
 devdocs: $(SLATE_GIT_HACK)
-	cd docs && bundle exec middleman server
+		cd docs && bundle exec middleman server
 
 docs: $(SLATE_GIT_HACK)
-	cd docs && rake build
+		cd docs && rake build
 
 commit:
-	-git commit -a && git push --all
+		-git commit -a && git push --all
 
 publish: commit docs
-	rm -rf $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/current
-	cp -r $(DOCS_BUILD_DIR) $(DOCS_PROD_DIR)/current
-	rm -rf $(DOCS_PROD_DIR)/*/.git
-	cd $(DOCS_PROD_DIR) && \
-	git init && \
-	git add * &> /dev/null && \
-	git commit -a -m "Generated content." &> /dev/null && \
-	git push -f $(REPO) master:gh-pages && \
-	cd - && \
-	cd $(DOCS_DIR) && \
-	rm .git
+		rm -rf $(DOCS_PROD_DIR)/.git $(DOCS_PROD_DIR)/current
+		cp -r $(DOCS_BUILD_DIR) $(DOCS_PROD_DIR)/current
+		rm -rf $(DOCS_PROD_DIR)/*/.git
+		cd $(DOCS_PROD_DIR) && \
+		git init && \
+		git add * &> /dev/null && \
+		git commit -a -m "Generated content." &> /dev/null && \
+		git push -f $(REPO) master:gh-pages && \
+		cd $(DOCS_DIR) && \
+		rm .git
